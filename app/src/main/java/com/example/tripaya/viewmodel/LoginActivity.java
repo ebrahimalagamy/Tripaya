47986be3720ad9ba3589b7836ecfe47b3a4b7764
Hossammohamed9 <hossam.mohamed8008@gmail.com>
2022-01-22 22:41:37 +0200

    User stay logged, changes in design

--- app/src/main/java/com/example/tripaya/viewmodel/LoginActivity.java
+++ app/src/main/java/com/example/tripaya/viewmodel/LoginActivity.java
@@ -2,15 +2,24 @@ package com.example.tripaya.viewmodel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.tripaya.Authentication.Login;
+import com.example.tripaya.MainActivity;
 import com.example.tripaya.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
+
+
 }
