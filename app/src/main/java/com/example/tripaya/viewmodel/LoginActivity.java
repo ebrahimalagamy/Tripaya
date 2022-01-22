0db243e1b003d4ede08e62ea0168a90f5f303589
Hossammohamed9 <hossam.mohamed8008@gmail.com>
2022-01-22 04:02:55 +0200

    ...

---
+++ app/src/main/java/com/example/tripaya/viewmodel/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.example.tripaya.viewmodel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tripaya.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    private  void forGit(){
+        //to update git
+    }
+}
